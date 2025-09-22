a5822d3b4cef4c3796456854a0d219905027c00d
HnHoussi <houcinehannoun@gmail.com>
2025-09-22 09:47:33 +0200

    test

--- cv-ai-backend/src/server.ts
+++ cv-ai-backend/src/server.ts
@@ -31,6 +31,6 @@ app.use("/api/coverLetter", coverLetterRoute);
 // Starting the server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Listening on port http://localhost:${port}`);
+    console.log(`Test : Listening on port http://localhost:${port}`);
 });
 
